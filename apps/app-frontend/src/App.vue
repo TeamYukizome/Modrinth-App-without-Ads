e9986bdce081fe3f151ffabe6655902f26945933
Team Yukizome <ichbinfreimc@hotmail.com>
2024-12-20 09:57:59 +0000

    Update App.vue
    Signed-off-by: Team Yukizome <ichbinfreimc@hotmail.com>

--- apps/app-frontend/src/App.vue
+++ apps/app-frontend/src/App.vue
@@ -258,9 +258,7 @@ async function logOut() {
 const MIDAS_BITFLAG = 1 << 0
 const hasPlus = computed(
   () =>
-    credentials.value &&
-    credentials.value.user &&
-    (credentials.value.user.badges & MIDAS_BITFLAG) === MIDAS_BITFLAG,
+    true,
 )
 
 watch(
